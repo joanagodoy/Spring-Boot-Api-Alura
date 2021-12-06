cd5cb897486bf2c4d3e2b51e5b3449159d42c303
joanagodoy <joanapereiragodoy@gmail.com>
2021-12-06 09:11:14 -0300

    Simplifica paginação com @EnableSpringDataWebSupport e adição de Pageable no parametro

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -1,9 +1,12 @@
 package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 public class ForumApplication {
 
 	public static void main(String[] args) {
